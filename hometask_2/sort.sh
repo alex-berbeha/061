41dfb3f779dd604672facd254c5e94c1beed985f
Berbeha Olexandr <Berbeha.olexandr@gmail.com>
2020-02-03 20:30:01 +0200

    update sort.sh

--- hometask_2/sort.sh
+++ hometask_2/sort.sh
@@ -3,4 +3,8 @@ if ! [ -f versions.txt ];
 then
 wget http://yoko.ukrtux.com:8899/versions.txt
 fi
-sort -V versions.txt
+sort -V versions.txt >> versions1.txt
+cat versions1.txt > versions.txt
+rm versions1.txt
+cat versions.txt
+
